e7e1fd45a5d5fdd442a319e58c9809bf12e30f08
Rida-fatima234 <ridafatima234890@gmail.com>
2024-04-23 16:07:15 +0500

    chapter no 1

---
+++ app.js
@@ -0,0 +1,4 @@
+alert("Error! please enter a valid password");
+alert("Welcome to JS Land...\n Happy Coding!");
+alert("Welcome to JS Land...");
+ alert(" Happy Coding! \n prevent this page from creating additional dialogs");
